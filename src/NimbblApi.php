f21d785bbdd5bb66f68746979b020be3d75a5b15
swati30 <swati@logicloop.io>
2021-11-30 01:06:45 +0530

    changes

--- src/NimbblApi.php
+++ src/NimbblApi.php
@@ -4,7 +4,7 @@ namespace Nimbbl\Api;
 
 class NimbblApi
 {
-    protected static $baseUrl = 'https://api.nimbbl.tech/api/';
+    protected static $baseUrl = 'https://uatapi.nimbbl.tech/api/';
 
     protected static $key;
 
@@ -30,10 +30,12 @@ class NimbblApi
      * @param string $key
      * @param string $secret
      */
-    public function __construct($key, $secret)
+    public function __construct($key, $secret, $url=null)
     {
         self::$key = $key;
         self::$secret = $secret;
+        if($url != null)
+            self::$baseUrl = $url;
     }
 
     /*
